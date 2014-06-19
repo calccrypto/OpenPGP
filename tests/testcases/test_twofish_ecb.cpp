2db199843b714c581c7cb6a4ddfdfc02c40f3e61
mugwort_rc <mugwort.rc@gmail.com>
2014-06-19 23:15:00 +0900

    Add Twofish KAT.

--- tests/testcases/test_twofish_ecb.cpp
+++ tests/testcases/test_twofish_ecb.cpp
@@ -2,9 +2,12 @@
 
 #include "Encryptions/Twofish.h"
 
-// Test vectors from <https://www.schneier.com/code/ecb_ival.txt>
+#include "testvectors/twofish/twofishecb_vk.h"
+#include "testvectors/twofish/twofishecb_vt.h"
 
-TEST(TwofishTest, test_twofish128_ecb) {
+// Test vectors from <https://www.schneier.com/code/twofish-kat.zip>
+
+TEST(TwofishTest, test_twofish128_ecb_ival) {
 
     std::string key(16, 0);
     std::string plain(16, 0);
@@ -15,7 +18,7 @@ TEST(TwofishTest, test_twofish128_ecb) {
     EXPECT_EQ(twofish.decrypt(cipher), plain);
 }
 
-TEST(TwofishTest, test_twofish192_ecb) {
+TEST(TwofishTest, test_twofish192_ecb_ival) {
 
     std::string key = unhexlify("0123456789ABCDEFFEDCBA98765432100011223344556677");
     std::string plain(16, 0);
@@ -27,7 +30,7 @@ TEST(TwofishTest, test_twofish192_ecb) {
 
 }
 
-TEST(TwofishTest, test_twofish256_ecb) {
+TEST(TwofishTest, test_twofish256_ecb_ival) {
 
     std::string key = unhexlify("0123456789ABCDEFFEDCBA987654321000112233445566778899AABBCCDDEEFF");
     std::string plain(16, 0);
@@ -39,12 +42,12 @@ TEST(TwofishTest, test_twofish256_ecb) {
 
 }
 
-TEST(TwofishTest, test_twofish128_ecb_full) {
+TEST(TwofishTest, test_twofish128_ecb_tbl) {
 
     std::string key(16, 0);
     std::string plain(16, 0);
 
-    for ( int i = 0; i < 49; ++i ) {
+    for ( unsigned int i = 0; i < 49; ++i ) {
         auto twofish = Twofish(key);
         key = plain;
         plain = twofish.encrypt(plain);
@@ -54,11 +57,11 @@ TEST(TwofishTest, test_twofish128_ecb_full) {
 
 }
 
-TEST(TwofishTest, test_twofish192_ecb_full) {
+TEST(TwofishTest, test_twofish192_ecb_tbl) {
     std::string key(24, 0);
     std::string plain(16, 0);
 
-    for ( int i = 0; i < 49; ++i ) {
+    for ( unsigned int i = 0; i < 49; ++i ) {
         auto twofish = Twofish(key);
         key = (plain + key).substr(0, 24);
         plain = twofish.encrypt(plain);
@@ -68,11 +71,11 @@ TEST(TwofishTest, test_twofish192_ecb_full) {
 
 }
 
-TEST(TwofishTest, test_twofish1256_ecb_full) {
+TEST(TwofishTest, test_twofish1256_ecb_tbl) {
     std::string key(32, 0);
     std::string plain(16, 0);
 
-    for ( int i = 0; i < 49; ++i ) {
+    for ( unsigned int i = 0; i < 49; ++i ) {
         auto twofish = Twofish(key);
         key = (plain + key).substr(0, 32);
         plain = twofish.encrypt(plain);
@@ -80,5 +83,83 @@ TEST(TwofishTest, test_twofish1256_ecb_full) {
 
     EXPECT_EQ(plain, unhexlify("37FE26FF1CF66175F5DDF4C33B97A205"));
 
+}
+
+TEST(TwofishTest, test_twofish128_ecb_vk) {
+
+    ASSERT_EQ(TWOFISH128_ECB_VK_KEY.size(), TWOFISH128_ECB_VK_CIPHER.size());
+
+    std::string UNHEX_TWOFISH_ECB_VK_PLAIN = unhexlify(TWOFISH_ECB_VK_PLAIN);
+
+    for ( unsigned int i = 0; i < TWOFISH128_ECB_VK_KEY.size(); ++i ) {
+        auto twofish128 = Twofish(unhexlify(TWOFISH128_ECB_VK_KEY[i]));
+        EXPECT_EQ(twofish128.encrypt(UNHEX_TWOFISH_ECB_VK_PLAIN), unhexlify(TWOFISH128_ECB_VK_CIPHER[i]));
+        EXPECT_EQ(twofish128.decrypt(unhexlify(TWOFISH128_ECB_VK_CIPHER[i])), UNHEX_TWOFISH_ECB_VK_PLAIN);
+    }
+}
+
+TEST(TwofishTest, test_twofish192_ecb_vk) {
+
+    ASSERT_EQ(TWOFISH192_ECB_VK_KEY.size(), TWOFISH192_ECB_VK_CIPHER.size());
+
+    std::string UNHEX_TWOFISH_ECB_VK_PLAIN = unhexlify(TWOFISH_ECB_VK_PLAIN);
+
+    for ( unsigned int i = 0; i < TWOFISH192_ECB_VK_KEY.size(); ++i ) {
+        auto twofish192 = Twofish(unhexlify(TWOFISH192_ECB_VK_KEY[i]));
+        EXPECT_EQ(twofish192.encrypt(UNHEX_TWOFISH_ECB_VK_PLAIN), unhexlify(TWOFISH192_ECB_VK_CIPHER[i]));
+        EXPECT_EQ(twofish192.decrypt(unhexlify(TWOFISH192_ECB_VK_CIPHER[i])), UNHEX_TWOFISH_ECB_VK_PLAIN);
+    }
+}
+
+TEST(TwofishTest, test_twofish256_ecb_vk) {
+
+    ASSERT_EQ(TWOFISH256_ECB_VK_KEY.size(), TWOFISH256_ECB_VK_CIPHER.size());
+
+    std::string UNHEX_TWOFISH_ECB_VK_PLAIN = unhexlify(TWOFISH_ECB_VK_PLAIN);
+
+    for ( unsigned int i = 0; i < TWOFISH256_ECB_VK_KEY.size(); ++i ) {
+        auto twofish256 = Twofish(unhexlify(TWOFISH256_ECB_VK_KEY[i]));
+        EXPECT_EQ(twofish256.encrypt(UNHEX_TWOFISH_ECB_VK_PLAIN), unhexlify(TWOFISH256_ECB_VK_CIPHER[i]));
+        EXPECT_EQ(twofish256.decrypt(unhexlify(TWOFISH256_ECB_VK_CIPHER[i])), UNHEX_TWOFISH_ECB_VK_PLAIN);
+    }
+}
+
+TEST(TwofishTest, test_twofish128_ecb_vt) {
+
+    ASSERT_EQ(TWOFISH_ECB_VT_PLAIN.size(), TWOFISH128_ECB_VT_CIPHER.size());
+
+    std::string UNHEX_TWOFISH128_ECB_VT_KEY = unhexlify(TWOFISH128_ECB_VT_KEY);
+
+    for ( unsigned int i = 0; i < TWOFISH_ECB_VT_PLAIN.size(); ++i ) {
+        auto twofish128 = Twofish(UNHEX_TWOFISH128_ECB_VT_KEY);
+        EXPECT_EQ(twofish128.encrypt(unhexlify(TWOFISH_ECB_VT_PLAIN[i])), unhexlify(TWOFISH128_ECB_VT_CIPHER[i]));
+        EXPECT_EQ(twofish128.decrypt(unhexlify(TWOFISH128_ECB_VT_CIPHER[i])), unhexlify(TWOFISH_ECB_VT_PLAIN[i]));
+    }
+}
+
+TEST(TwofishTest, test_twofish192_ecb_vt) {
+
+    ASSERT_EQ(TWOFISH_ECB_VT_PLAIN.size(), TWOFISH192_ECB_VT_CIPHER.size());
+
+    std::string UNHEX_TWOFISH192_ECB_VT_KEY = unhexlify(TWOFISH192_ECB_VT_KEY);
+
+    for ( unsigned int i = 0; i < TWOFISH_ECB_VT_PLAIN.size(); ++i ) {
+        auto twofish192 = Twofish(UNHEX_TWOFISH192_ECB_VT_KEY);
+        EXPECT_EQ(twofish192.encrypt(unhexlify(TWOFISH_ECB_VT_PLAIN[i])), unhexlify(TWOFISH192_ECB_VT_CIPHER[i]));
+        EXPECT_EQ(twofish192.decrypt(unhexlify(TWOFISH192_ECB_VT_CIPHER[i])), unhexlify(TWOFISH_ECB_VT_PLAIN[i]));
+    }
+}
+
+TEST(TwofishTest, test_twofish256_ecb_vt) {
+
+    ASSERT_EQ(TWOFISH_ECB_VT_PLAIN.size(), TWOFISH256_ECB_VT_CIPHER.size());
+
+    std::string UNHEX_TWOFISH256_ECB_VT_KEY = unhexlify(TWOFISH256_ECB_VT_KEY);
+
+    for ( unsigned int i = 0; i < TWOFISH_ECB_VT_PLAIN.size(); ++i ) {
+        auto twofish256 = Twofish(UNHEX_TWOFISH256_ECB_VT_KEY);
+        EXPECT_EQ(twofish256.encrypt(unhexlify(TWOFISH_ECB_VT_PLAIN[i])), unhexlify(TWOFISH256_ECB_VT_CIPHER[i]));
+        EXPECT_EQ(twofish256.decrypt(unhexlify(TWOFISH256_ECB_VT_CIPHER[i])), unhexlify(TWOFISH_ECB_VT_PLAIN[i]));
+    }
 }
 
