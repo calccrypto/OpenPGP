cde5b8407777e3581085baff8e49f18b16efd125
Jason Lee <calccrypto@gmail.com>
2017-03-02 21:41:01 -0500

    Forgot to add tests/testcases/objects.mk

---
+++ tests/testcases/objects.mk
@@ -0,0 +1,20 @@
+TESTCASES_OBJECTS=test_aes128_ecb.o             \
+                  test_aes192_ecb.o             \
+                  test_aes256_ecb.o             \
+                  test_blowfish_ecb.o           \
+                  test_camellia_ecb.o           \
+                  test_cast128_ecb.o            \
+                  test_idea_ecb.o               \
+                  test_tripledes_ecb.o          \
+                  test_twofish_ecb.o            \
+                  test_md5.o test_ripemd160.o   \
+                  test_sha1.o                   \
+                  test_sha224.o                 \
+                  test_sha256.o                 \
+                  test_sha384.o                 \
+                  test_sha512.o                 \
+                  test_dsa.o                    \
+                  test_rsa.o                    \
+                  test_compress.o               \
+                  test_pgp.o                    \
+                  test_radix64.o
