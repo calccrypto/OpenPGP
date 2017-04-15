ca2c4841cdd12d5f3428ab9ce80fbfa1ea9937f9
Jason Lee <calccrypto@gmail.com>
2017-04-15 16:42:58 -0400

    forgot to add gtest.sh

---
+++ gtest.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# script to download and build GoogleTest
+
+# not much better than git submodules, but there was never a need/want for the repo in this repo
+cd ..
+git clone https://github.com/google/googletest.git
+cd googletest
+git reset --hard d62d6c6556d96dda924382547c54a4b3afedb22c
+cmake CMakeLists.txt
+make
+cd ../OpenPGP/tests
+make debug
