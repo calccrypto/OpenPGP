bcc48a5126a8054f517a97ac1e992c7952074057
Jason Lee <calccrypto@gmail.com>
2017-04-23 23:01:08 -0400

    move directories

--- tests/gtest.sh
+++ tests/gtest.sh
@@ -3,7 +3,7 @@
 # script to download and build GoogleTest
 # not much better than git submodules, but there was never a need/want for the repo in this repo
 
-cd ..
+cd ../..
 git clone https://github.com/google/googletest.git
 cd googletest
 git reset --hard d62d6c6556d96dda924382547c54a4b3afedb22c
