35a267c7db289cc1a56f741c2bf56c5bb21042e4
D-o-c <mpini91@gmail.com>
2017-10-12 15:47:44 +0200

    Added time with generic format (removed length array)

--- Misc/pgptime.cpp
+++ Misc/pgptime.cpp
@@ -25,6 +25,13 @@ std::string show_time(time_t time){
     return date.str();
 }
 
+// write a time following strftime format
+std::string show_time_format(time_t time, const char* format = "%F %T", uint8_t limit = 80){
+        char *buffer = new char(limit);
+        strftime (buffer, limit, format, localtime (&time));
+        return buffer;
+}
+
 std::string show_date(time_t time){
     struct tm * gmt = gmtime(&time);
     std::stringstream date;
