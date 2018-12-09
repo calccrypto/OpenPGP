b5c3203916e979ce979573ae530048f23884e7c4
Jason Lee <calccrypto@gmail.com>
2018-12-08 20:36:32 -0700

    updated Google Test repository

--- tests/gtest.sh
+++ tests/gtest.sh
@@ -4,8 +4,7 @@
 # not much better than git submodules, but there was never a need/want for the repo in this repo
 
 cd ../..
-git clone https://github.com/google/googletest.git
+git clone --depth=1 https://github.com/abseil/googletest.git
 cd googletest
-git reset --hard d62d6c6556d96dda924382547c54a4b3afedb22c
 cmake CMakeLists.txt
 make
